7f58b702cd637c6b6ec09f3114909b62f302b84b
psypasta <robin.van.manen@hotmail.com>
2018-10-20 22:35:09 +0200

    login component and testing - max

--- ObamaClient/src/app/login/login.component.spec.ts
+++ ObamaClient/src/app/login/login.component.spec.ts
@@ -1,26 +1,47 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
-import {AppComponent} from '../app.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
-describe('LoginComponent', () => {
+describe('Component: Login', () => {
+
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
-  }));
-
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule, FormsModule],
+      declarations: [LoginComponent]
+    });
+
+    // create component and test fixture
     fixture = TestBed.createComponent(LoginComponent);
+//  fixture.detectChanges();
+
+    // get test component from the fixture
     component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    component.ngOnInit();
+  }); //  .compileComponents();
+
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('form invalid when empty', () => {
+    expect(component.form.valid).toBeFalsy();
+  });
+
+  it('email field validity', () => {
+    const email = component.form.controls['email'];
+    expect(email.valid).toBeFalsy();
+  });
+
+  it('email field validity', () => {
+    let errors;
+    const email = component.form.controls['email'];
+    errors = email.errors || {};
+    expect(errors['required']).toBeTruthy();
+  });
+
 });
--- ObamaClient/src/app/login/login.component.ts
+++ ObamaClient/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {User} from '../model/user';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  @Output() loggedIn = new EventEmitter<User>();
+  form: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private fb: FormBuilder) {
   }
 
+  ngOnInit() {
+    this.form = this.fb.group({
+      email: ['', [
+        Validators.required,
+        Validators.pattern('[^ @]*@[^ @]*')]],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(8)]],
+    });
+  }
+
+  login() {
+    console.log(`Login ${this.form.value}`);
+    if (this.form.valid) {
+      this.loggedIn.emit(
+        new User(
+          this.form.value.email,
+          this.form.value.password
+        )
+      );
+    }
+  }
 }
